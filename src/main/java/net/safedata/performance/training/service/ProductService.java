4a3fd47dba12151f51a9302fbdbaffe83aeacab6
Bogdan Solga <bogdan.solga@gmail.com>
2024-05-21 13:12:34 +0300

    [improve] Integrated a StopWatch, refined the memory displaying

--- src/main/java/net/safedata/performance/training/service/ProductService.java
+++ src/main/java/net/safedata/performance/training/service/ProductService.java
@@ -5,14 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
@@ -32,68 +28,95 @@ public class ProductService {
     private double totalSales = 0;
     private final List<Product> products = new ArrayList<>();
 
+    /*
     @Scheduled(
             fixedRate = 5,
             timeUnit = TimeUnit.SECONDS
     )
+    */
     public void simulateProductsProcessing() {
         System.out.println();
 
-        final long memoryBefore = getFreeMemory() / BYTES_IN_MB;
+        final long memoryBefore = getFreeMemoryInMB();
         LOGGER.info("JVM memory in use before: {} MB", memoryBefore);
 
         final long now = System.currentTimeMillis();
         processALotOfProducts();
         LOGGER.info("The entire processing took {} ms", System.currentTimeMillis() - now);
 
-        final long memoryAfter = getFreeMemory() / BYTES_IN_MB;
+        final long memoryAfter = getFreeMemoryInMB();
         LOGGER.info("JVM memory in use after: {} MB", memoryAfter);
+    }
 
-        // double brace init - potential cause for a memory leak
-        new HashMap<Integer, Product>() {{
-            IntStream.range(0, 200)
-                     .forEach(index -> put(index, buildProduct(RANDOM.nextInt())));
-        }};
+    @Scheduled(
+            fixedRate = 5,
+            timeUnit = TimeUnit.SECONDS
+    )
+    public void simulateProductsProcessingUsingAStopwatch() {
+        System.out.println();
 
-        { // inner / private / anonymous block
-            @SuppressWarnings("unused")
-            final Product product = buildProduct(20);
-        }
+        StopWatch stopWatch = new StopWatch("simulateProductsProcessing");
+
+        final long memoryBefore = getFreeMemoryInMB();
+        LOGGER.info("JVM memory in use before the processing: {} MB", memoryBefore);
+
+        stopWatch.start("Processing a lot of products");
+        processALotOfProducts();
+        stopWatch.stop();
+
+        stopWatch.start("A short expensive task");
+        sleepALittle(200);
+        stopWatch.stop();
+
+        stopWatch.start("A long expensive task");
+        sleepALittle(5000);
+        stopWatch.stop();
+
+        final long memoryAfter = getFreeMemoryInMB();
+        LOGGER.info("JVM memory in use after the processing: {} MB", memoryAfter);
+
+        LOGGER.info("Execution summary: {}", stopWatch.prettyPrint());
     }
 
     private void processALotOfProducts() {
-        final int size = RANDOM.nextInt(500000);
-        IntStream.range(0, size)
-                 .forEach(index -> products.add(buildProduct(index)));
+        final int productsNumber = RANDOM.nextInt(1000); //000
+        generateProducts(productsNumber);
 
-        final double totalPrice = products.stream()
-                                          .mapToDouble(Product::getPrice)
-                                          .sum();
+        final double totalPrice = getProductsPriceSum(products);
         LOGGER.info("The total price of {} products is {}", products.size(), decimalFormat.format(totalPrice));
 
         totalSales += totalPrice;
+        LOGGER.info("The total sales is currently {}", decimalFormat.format(totalSales));
+    }
+
+    private void generateProducts(int productsNumber) {
+        IntStream.rangeClosed(0, productsNumber)
+                 .forEach(index -> products.add(buildProduct(index)));
     }
 
     //@Scheduled(fixedRate = 5000)
     public void generateALotOfData() {
-        final int howMany = 1_000_000;
-
-        final long memoryBefore = getFreeMemory() / BYTES_IN_MB;
+        final long memoryBefore = getFreeMemoryInMB();
         LOGGER.info("JVM memory in use before generating a lot of data: {} MB", memoryBefore);
 
-        final Set<Product> products = new HashSet<>(howMany);
-        IntStream.range(0, howMany)
-                 .forEach(index -> products.add(buildProduct(index)));
-        LOGGER.info("The products cost: {}", products.stream()
-                                                     .mapToDouble(Product::getPrice)
-                                                     .sum());
+        final int productsNumber = 1_000_000;
+        final Set<Product> innerProducts = new HashSet<>(productsNumber);
+        generateProducts(productsNumber);
+        LOGGER.info("The products cost: {}", getProductsPriceSum(innerProducts));
 
-        final long memoryAfter = getFreeMemory() / BYTES_IN_MB;
+        final long memoryAfter = getFreeMemoryInMB();
         LOGGER.info("JVM memory in use after generating a lot of data: {} MB", memoryAfter);
     }
 
-    private long getFreeMemory() {
-        return RUNTIME.totalMemory() - RUNTIME.freeMemory();
+    private static double getProductsPriceSum(Collection<Product> products) {
+        //TODO replace with StreamSupport.parallel
+        return products.stream()
+                       .mapToDouble(Product::getPrice)
+                       .sum();
+    }
+
+    private long getFreeMemoryInMB() {
+        return (RUNTIME.totalMemory() - RUNTIME.freeMemory()) / BYTES_IN_MB;
     }
 
     public double getTotalSales() {
@@ -118,16 +141,30 @@ public class ProductService {
     }
 
     private Product buildProduct(final int index) {
-        sleepALittle(50);
+        sleepALittle(10);
         return new Product(index, "The product " + index, 1000 * RANDOM.nextInt(50000));
     }
 
     private void sleepALittle(final int bound) {
-        if (true) return; //TODO uncomment to add some processing time
+        //if (true) return; //TODO uncomment to add some processing time
         try {
-            Thread.sleep(RANDOM.nextInt(Math.abs(bound) + 10));
+            Thread.sleep(RANDOM.nextInt(Math.abs(bound)));
         } catch (InterruptedException e) {
             LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public void potentialMemoryLeaksGenerators() {
+        // double brace init - potential cause for a memory leak
+        new HashMap<Integer, Product>() {{
+            IntStream.range(0, 200)
+                     .forEach(index -> put(index, buildProduct(RANDOM.nextInt())));
+        }};
+
+        { // inner / private / anonymous block
+            @SuppressWarnings("unused")
+            final Product product = buildProduct(20);
         }
     }
 }
