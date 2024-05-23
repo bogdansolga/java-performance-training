200c9928abdefbbb2c65420137408a900b023bd5
Bogdan Solga <bogdan.solga@gmail.com>
2024-05-23 13:10:53 +0300

    [dev] Integrated an Oracle database

---
+++ src/main/java/net/safedata/performance/training/domain/model/Product.java
@@ -0,0 +1,49 @@
+package net.safedata.performance.training.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "price", length = 2)
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
